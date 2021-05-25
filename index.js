880ccbf1c8115683a25be90203bff85c14cd2175
Juan Carlos <juancarloscanoruiz7@gmail.com>
2021-05-25 09:08:24 -0500

    cors added

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ const sequelize = require('./util/db');
 const bodyParser = require('body-parser');
 const postsRouter = require('./routes/posts');
 const Posts = require('./Models/Post');
+const cors = require('cors');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const port = process.env.PORT || '3000';
+const port = process.env.PORT || '5000';
 
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use("/", postsRouter);
